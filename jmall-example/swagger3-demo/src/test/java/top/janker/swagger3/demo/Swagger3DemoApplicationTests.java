aef3d32ebd06a02a9eebe54eea07705913cf8013
janker <690864766@qq.com>
2023-12-30 17:52:59 +0800

    升级jdk17

---
+++ jmall-example/swagger3-demo/src/test/java/top/janker/swagger3/demo/Swagger3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package top.janker.swagger3.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swagger3DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
